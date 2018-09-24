bfba91e9b87f5a3b9d7118ea4d9a59fbdafab5fd
Louis Bur <louis@motionlead.com>
2018-09-24 10:16:03 -0400

    Updated for swift 4.2

--- SwiftWebViewBridge/SwiftWebViewBridge.swift
+++ SwiftWebViewBridge/SwiftWebViewBridge.swift
@@ -298,7 +298,7 @@ extension SwiftWebViewBridge {
 extension SwiftWebViewBridge: UIWebViewDelegate {
     
     // It's the only entrance where JavaScript can call Swift/ObjC handler or callback.Every URL loading in any frames will trigger this method
-    public func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    public func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
         
         if let url:URL = request.url {
             
